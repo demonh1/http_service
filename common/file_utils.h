566d1a6fe234766cf1b3abcca5179ec45cd185d9
demonh1 <demon7987@gmail.com>
2017-03-14 20:12:39 +0300

    Update file_utils.h

--- common/file_utils.h
+++ common/file_utils.h
@@ -6,32 +6,32 @@
 
 namespace file_hlp {
 
-template< typename vector_t > inline bool LoadFile( const std::string& strUtf8FileName, vector_t& vecDest, const std::locale& locale = std::locale("") )
+template<typename vector_t> inline bool LoadFile( const std::string& strUtf8FileName, vector_t& vecDest, const std::locale& locale = std::locale("") )
 {
     vecDest.clear();
 
     boost::filesystem::path filePath( strUtf8FileName );
 
-    if( !boost::filesystem::exists( filePath ) )
+    if( !boost::filesystem::exists(filePath) )
     {
         return false;
     }
 
-    std::basic_ifstream< typename vector_t::value_type > ifsFile( strUtf8FileName.c_str(), std::ios_base::in | std::ios_base::binary );
+    std::basic_ifstream<typename vector_t::value_type> ifsFile( strUtf8FileName.c_str(), std::ios_base::in | std::ios_base::binary );
 
     if( !ifsFile )
     {
         return false;
     }
 
-    ifsFile.imbue( locale );
+    ifsFile.imbue(locale);
 
     boost::uintmax_t fileSize = boost::filesystem::file_size( filePath );
 
     if( 0 != fileSize )
     {
-        vecDest.resize( static_cast< size_t >( fileSize ) );
-        if( !ifsFile.read( &vecDest[ 0 ], static_cast< std::streamsize >( fileSize ) ) )
+        vecDest.resize( static_cast< size_t >(fileSize) );
+        if( !ifsFile.read( &vecDest[0], static_cast< std::streamsize >( fileSize ) ) )
         {
             vecDest.clear();
             return false;
